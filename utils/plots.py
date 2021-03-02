e7bcfc61012769d232973153696ff0fe86867f60
NPilis <norbertpilis@gmail.com>
2021-03-02 13:48:33 +0100

    add clean notebook for simple cnn training

--- utils/plots.py
+++ utils/plots.py
@@ -9,18 +9,17 @@ def plot_face_with_label(face, label, possible_emotions=None):
         label = possible_emotions[label_idx]
     ax.set_title(label)
 
-def plot_model_history(num_epochs, history):
-    epochs_range = range(num_epochs)
+def plot_model_history(history):
     train_acc = history['accuracy']
     val_acc = history['val_accuracy']
     train_loss = history['loss']
     val_loss = history['val_loss']
 
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10,5))
-    ax1.plot(epochs_range, train_acc, label='Training Accuracy')
-    ax1.plot(epochs_range, val_acc, label='Validation Accuracy')
-    ax2.plot(epochs_range, train_loss, label='Training Loss')
-    ax2.plot(epochs_range, val_loss, label='Validation Loss')
+    ax1.plot(train_acc, label='Training Accuracy')
+    ax1.plot(val_acc, label='Validation Accuracy')
+    ax2.plot(train_loss, label='Training Loss')
+    ax2.plot(val_loss, label='Validation Loss')
     ax1.set_title("Model accuracy")
     ax2.set_title("Model loss")
     ax1.legend()
